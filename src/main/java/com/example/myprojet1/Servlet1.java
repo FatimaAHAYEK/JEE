e92d60ce4491b123bcf17600e7b9b4b6d17d9f1a
FatimaAHAYEK <ahaykfatima123@gmail.com>
2024-03-07 14:20:06 +0100

    Mycommit4

--- src/main/java/com/example/myprojet1/Servlet1.java
+++ src/main/java/com/example/myprojet1/Servlet1.java
@@ -19,13 +19,19 @@ public class Servlet1 extends HttpServlet {
         PrintWriter out = response.getWriter();
         String Nom = request.getParameter("Nom");
         out.println("<html><body>");
-        out.println("<form action='Servlet2' methode='post'>");
-        out.println("Hello"+Nom);
+        //body ona l'action to the servlet2 avec une methode Post
+        out.println("<form action='Servlet2' method='post'>");
+        // affichage de la varibale nom
+        out.println("Welcome" + Nom);
+        out.println("</br>");
         out.println("<input type='submit' value='Go'>");
         out.println("</form>");
         out.println("</body></html>");
-
+        //un cookie comme une petite unité de stockage côté client qui peut être utilisée pour stocker des informations spécifiques.
+        // creation d'un nouvel objet cookie avec le nom "Nom"
         Cookie Mycookie = new Cookie("Nom", Nom);
+        //le cookie sera inclus dans la réponse envoyée au navigateur du client
+        //Une fois que le navigateur reçoit ce cookie, il le stockera localement et l'inclura dans les futures demandes vers le même domaine.
         response.addCookie(Mycookie);
     }
 }
--- src/main/java/com/example/myprojet1/Servlet2.java
+++ src/main/java/com/example/myprojet1/Servlet2.java
@@ -12,24 +12,42 @@ public class Servlet2 extends HttpServlet {
     public int i;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (int i = 0; i < cookies.length; i++) {
-                String name = cookies[i].getName();
-                String value = cookies[i].getValue();
-                out.println("Cookie Name: " + name + "<br>");
-                out.println("Cookie Value: " + value + "<br>");
-            }
-        } else {
-            out.println("No cookies found");
-        }
-        out.close();
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null && cookies.length > 0) {
+            String nomValue = null;
+
+            // Recherche du cookie avec le nom "Nom"
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("Nom")) {
+                    nomValue = cookie.getValue();
+                    break;
+                }
+            }
+
+            if (nomValue != null) {
+                out.println("<html><body>");
+                out.println("Hello " + nomValue);
+                out.println("</body></html>");
+            } else {
+                out.println("<html><body>");
+                out.println("No 'Nom' cookie found");
+                out.println("</body></html>");
+            }
+        } else {
+            // Aucun cookie trouvé, mais pas de message affiché
+            out.println("<html><body>");
+            out.println("</body></html>");
+        }
+
+        out.close();
 
     }
 }
